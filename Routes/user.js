3d59ec560851322d906f31ff37ee65398b2b2cd7
hashmat1530 <hashmat2020vskp@gmail.com>
2024-05-21 15:24:35 +0530

    grid bugs fixed

--- Routes/user.js
+++ Routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../Models/user')
-const router = express.Router();
+const router = express.Router()
 
 router.get('/signin',(req,res)=>{
     res.render('signin.ejs')
